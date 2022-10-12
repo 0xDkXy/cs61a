1081c00f6fe25a7dabe048116b2ca9bacced475d
0xDkXy <bo001015@outlook.com>
2022-10-12 21:47:25 +0800

    add: complete project cats

--- push.sh
+++ push.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 red="\e[0;31m"
 green="\e[0;32m"
+default_color="\e[00m"
 git pull origin master
 if [ $? -eq 1 ];then
-    echo -e "${red}Fail to pull"
+    echo -e "${red}Fail to pull${default_color}"
 else
-    echo -e "${green}Successfully pull"
+    echo -e "${green}Successfully pull${default_color}"
     git add .
     git commit -m "$1"
     git push origin master
     if [ $? -eq 0 ];then
-        echo -e "${green}Successfully push"
+        echo -e "${green}Successfully push${default_color}"
     else
-        echo -e "${red}Fail to push"
+        echo -e "${red}Fail to push${default_color}"
     fi
 fi
     
